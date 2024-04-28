eaae07360b1e1031f4e6f87080a9b5203e097644
CauaSilva28 <caua.santos56@etec.sp.gov.br>
2024-04-28 00:19:31 -0300

    add modo light e dark

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const toggleTheme = document.querySelector('#toggletheme');
+const rootHtml = document.documentElement;
+
 const capa = document.getElementById('imagemcapa');
 var num = 1;
 
@@ -23,3 +26,20 @@ setInterval(function(){
     }
     capa.style.backgroundImage = "url(img/capaaventura" + num + ".jpg)";
 },5000);
+
+//Alternar modo light e dark
+function changeTheme(){
+    const currentTheme = rootHtml.getAttribute('data-theme');
+
+    if (currentTheme === 'dark') {
+        rootHtml.setAttribute('data-theme', 'light');
+    } 
+    else {
+        rootHtml.setAttribute('data-theme', 'dark');
+    }
+
+    toggleTheme.classList.toggle('bi-moon-fill');
+    toggleTheme.classList.toggle('bi-sun');
+}
+
+toggleTheme.addEventListener('click', changeTheme);
