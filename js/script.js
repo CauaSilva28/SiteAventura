af737bd5c738fa875dff17451c37dd48faeec2ea
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2024-06-22 23:52:21 -0300

    Atualizando animações, layout e nova página

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+AOS.init();
+
 const toggleTheme = document.querySelector('#toggletheme');
 const rootHtml = document.documentElement;
 
@@ -81,21 +83,3 @@ function changeTheme(){
 }
 
 toggleTheme.addEventListener('click', changeTheme);
-
-//Transicao aparecer scroll
-const myObserver = new IntersectionObserver(function(entries){
-    entries.forEach(function(entry) {
-        if(entry.isIntersecting){
-            entry.target.classList.add('transicao');
-        }
-        else{
-            entry.target.classList.remove('transicao');
-        }
-    });
-});
-
-const elements = document.querySelectorAll('.localaparecer');
-const elements2 = document.querySelectorAll('.localsurgir');
-
-elements.forEach((element) => myObserver.observe(element));
-elements2.forEach((element) => myObserver.observe(element));
