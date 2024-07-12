8197c868073443b69f59f21fa32c3b37a7ce7445
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2024-07-12 00:04:49 -0300

    Atualizando elementos e tela de venda

---
+++ js/aventura.js
@@ -0,0 +1,10 @@
+AOS.init();
+
+const telaVenda = document.getElementById('fundoTelaVenda');
+
+function abrirTelaVenda(){
+    telaVenda.style.display = 'flex';
+}
+function fecharTelaVenda(){
+    telaVenda.style.display = 'none';
+}
