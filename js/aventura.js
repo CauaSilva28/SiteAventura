e6664eb2d5bc3f67a3f2a15a14b1f1befbd16ce6
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2024-07-22 18:07:03 -0300

    Criando diferentes elementos para a tela de venda

--- js/aventura.js
+++ js/aventura.js
@@ -2,9 +2,25 @@ AOS.init();
 
 const telaVenda = document.getElementById('fundoTelaVenda');
 
-function abrirTelaVenda(){
-    telaVenda.style.display = 'flex';
-}
 function fecharTelaVenda(){
     telaVenda.style.display = 'none';
+}
+
+const catalogos = document.querySelectorAll('.catalogos');
+const imgTelaVenda = document.getElementById('telaVenda-img');
+const telaVendaTitulo = document.getElementById('telaVenda-title');
+
+const titulosTelaVenda = [
+    'Passeio de cruzeiro oceano',
+    'Passeio de iate mares',
+    'Passeio de escuna mares',
+    'Passeio de veleiro rios',
+];
+
+for (let i = 0; i < catalogos.length; i++) {
+    catalogos[i].onclick = function() {
+        telaVenda.style.display = 'flex';
+        imgTelaVenda.style.backgroundImage = 'url(../img/imgpacotes/imgTelaVenda/imgvenda' + i + '.jpg)';
+        telaVendaTitulo.textContent = titulosTelaVenda[i];
+    };
 }
